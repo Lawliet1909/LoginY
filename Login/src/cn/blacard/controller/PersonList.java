7740d63f60b31af4f5543d10c885df7766b02b95
lawliet1909 <849010877@qq.com>
2016-09-04 18:14:07 +0800

    first version1.01

--- Login/src/cn/blacard/controller/PersonList.java
+++ Login/src/cn/blacard/controller/PersonList.java
@@ -6,11 +6,9 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import cn.blacard.entity.Person;
 import cn.blacard.service.LoginService;
 
